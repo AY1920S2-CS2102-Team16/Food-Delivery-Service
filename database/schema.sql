8f4b3093d52c397c48a48b5e8fc39e99233920b2
liujiajun <liujiajun1999@gmail.com>
2020-03-18 00:33:39 +0800

    Separate trigger and schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 create extension if not exists cube;
 create extension if not exists earthdistance;
 
-drop table if exists Users, Managers, Customers, Restaurants, Riders, Sells, CustomerLocations, Orders, OrderFoods, Delivers cascade;
+drop table if exists Users, Managers, Customers, Restaurants, Riders, Sells, CustomerLocations, Orders, OrderFoods cascade;
 drop type if exists food_category_t, delivery_rating_t;
 
 create type food_category_t AS ENUM ('Chinese', 'Western', 'Malay', 'Indian', 'Fast food');
@@ -17,8 +17,7 @@ create table Users
 
 create table Managers
 (
-    id varchar(20) primary key references Users (id)
-        on delete cascade
+    id varchar(20) primary key references Users (id) on delete cascade
 );
 
 create table Restaurants
@@ -33,14 +32,12 @@ create table Restaurants
 
 create table Customers
 (
-    id varchar(20) primary key references Users (id)
-        on delete cascade
+    id varchar(20) primary key references Users (id) on delete cascade
 );
 
 create table Riders
 (
-    id varchar(20) primary key references Users (id)
-        on delete cascade
+    id varchar(20) primary key references Users (id) on delete cascade
 );
 
 create table Sells
@@ -53,11 +50,16 @@ create table Sells
     daily_sold       integer         not null default 0,
     price            money           not null check (price >= 0::money),
 
-    constraint constraint_daily_limit check (daily_limit >= 0 and daily_limit >= daily_sold),
-    constraint constraint_daily_sold check (daily_sold >= 0 and daily_sold <= daily_limit),
+    constraint daily_limit check (daily_limit >= 0 and daily_limit >= daily_sold),
+    constraint daily_sold check (daily_sold >= 0),
     primary key (rid, food_name)
 );
 
+/*
+ Customers' delivery addresses.
+ Guarantees: Each customer can have at most 5 locations (by tr_ensure_maximum_recent_location).
+             Least recent used locations will be removed if the locations for a customer are full and new locations are added.
+ */
 create table CustomerLocations
 (
     cid            varchar(20),
@@ -65,157 +67,50 @@ create table CustomerLocations
     lat            float check (fn_check_lat(lat)),
     address        varchar(100) not null,
     last_used_time timestamp    not null default CURRENT_TIMESTAMP,
+
+    foreign key (cid) references Customers (id) on delete cascade,
     primary key (cid, lon, lat)
 );
 
 create table Orders
 (
-    id            serial,
-    delivery_cost money not null check (delivery_cost >= 0::money),
-    food_cost     money not null check (food_cost >= 0::money),
+    id             serial,
+    delivery_cost  money       not null check (delivery_cost >= 0::money),
+    food_cost      money       not null check (food_cost >= 0::money),
 
-    primary key (id)
-);
-
-create table OrderFoods
-(
-    rid       varchar(20),
-    food_name varchar(50),
-    oid       integer references Orders (id),
-    quantity  integer not null,
-
-    foreign key (rid, food_name) references Sells (rid, food_name),
-    primary key (oid, rid, food_name)
-);
-
-create table Delivers
-(
-    oid            integer references Orders (id),
-    rid            varchar(20) not null references Riders (id),
+    -- delivery information
+    rider_id       varchar(20) not null references Riders (id),
     cid            varchar(20) not null,
     lon            float       not null check (fn_check_lon(lon)),
     lat            float       not null check (fn_check_lat(lat)),
 
+    -- timing information
+    time_placed    timestamp   not null default CURRENT_TIMESTAMP,
     time_depart    timestamp,
     time_collect   timestamp,
     time_leave     timestamp,
     time_delivered timestamp,
+
     rating         delivery_rating_t,
 
-    primary key (oid),
-    foreign key (cid, lon, lat) references CustomerLocations (cid, lon, lat)
+    foreign key (cid, lon, lat) references CustomerLocations (cid, lon, lat) on delete set null,
+    primary key (id)
 );
 
-
 /*
- Updates daily sold food items whenever an order is placed or updated.
+ Food items of orders.
+ Guarantees: All food items for a single order is from the same restaurant (by tr_order_food_from_same_restaurant).
  */
-drop trigger if exists tr_update_daily_sold on OrderFoods cascade;
-create trigger tr_update_daily_sold
-    before insert or update
-    on OrderFoods
-    for each row
-execute function increase_daily_sold();
+create table OrderFoods
+(
+    rid       varchar(20),
+    oid       integer references Orders (id) on delete cascade,
+    food_name varchar(50),
+    quantity  integer not null,
 
-create or replace function increase_daily_sold() returns trigger as
-$$
-begin
-    update Sells
-    set daily_sold = daily_sold + new.quantity - (case when old.quantity is null then 0 else old.quantity end)
-    where rid = new.rid
-      and food_name = new.food_name;
-    return new;
-end;
-$$ language plpgsql;
-
-/*
-  Ensures only the number of location for each customer dose not exceed maximum number. If attempting to insert
-  after reaching the maximum, the least recently used location will be removed.
- */
-drop trigger if exists tr_ensure_maximum_recent_location on CustomerLocations cascade;
-create trigger tr_ensure_maximum_recent_location
-    after insert
-    on CustomerLocations
-    for each row
-execute function ensure_maximum_recent_location();
-
-create or replace function ensure_maximum_recent_location() returns trigger as
-$$
-begin
-    delete
-    from CustomerLocations
-    where (cid, lat, lon) in (
-        select cid, lat, lon
-        from CustomerLocations
-        where cid = new.cid
-        order by last_used_time desc
-            offset 5
-        limit 1);
-    return null;
-end ;
-$$ language plpgsql;
-
-/**
-  Ensures that non-overlapping and covering ISA relationship between Users and different user roles.
- */
-create or replace function fn_ensure_covering_and_non_overlapping_roles() returns trigger as
-$$
-begin
-    if (select count(*)
-        from (select id
-              from Customers
-              union
-              select id
-              from Restaurants
-              union
-              select id
-              from Riders
-              union
-              select id
-              from Managers) as IDS
-        where IDS.id = new.id
-       ) = 1 then
-        return null;
-    end if;
-    raise exception 'User id % already has an other role or is not given a role.', new.id;
-end;
-$$ language plpgsql;
-
-drop trigger if exists tr_users_covering_role on Users cascade;
-create constraint trigger tr_users_covering_role
-    after insert or update
-    on Users
-    deferrable initially deferred
-    for each row
-execute function fn_ensure_covering_and_non_overlapping_roles();
-
-drop trigger if exists tr_restaurants_covering_role on Restaurants cascade;
-create trigger tr_restaurants_covering_role
-    after insert or update
-    on Restaurants
-    for each row
-execute function fn_ensure_covering_and_non_overlapping_roles();
-
-drop trigger if exists tr_customers_covering_role on Customers cascade;
-create trigger tr_customers_covering_role
-    after insert or update
-    on Customers
-    for each row
-execute function fn_ensure_covering_and_non_overlapping_roles();
-
-drop trigger if exists tr_riders_covering_role on Riders cascade;
-create trigger tr_riders_covering_role
-    after insert or update
-    on Riders
-    for each row
-execute function fn_ensure_covering_and_non_overlapping_roles();
-
-drop trigger if exists tr_managers_covering_role on Managers cascade;
-create trigger tr_managers_covering_role
-    after insert or update
-    on Managers
-    for each row
-execute function fn_ensure_covering_and_non_overlapping_roles();
+    foreign key (rid, food_name) references Sells (rid, food_name) on delete set null,
+    primary key (oid, rid, food_name)
+);
 
 create or replace function fn_check_lon(lon float) returns boolean as
 $$
---
+++ database/triggers.sql
@@ -0,0 +1,129 @@
+/*
+ Updates daily sold food items whenever an order is placed or updated.
+ */
+drop trigger if exists tr_update_daily_sold on OrderFoods cascade;
+create trigger tr_update_daily_sold
+    before insert or update
+    on OrderFoods
+    for each row
+execute function increase_daily_sold();
+
+create or replace function increase_daily_sold() returns trigger as
+$$
+begin
+    update Sells
+    set daily_sold = daily_sold + new.quantity - (case when old.quantity is null then 0 else old.quantity end)
+    where rid = new.rid
+      and food_name = new.food_name;
+    return new;
+end;
+$$ language plpgsql;
+
+/*
+  Ensures only the number of location for each customer dose not exceed maximum number. If attempting to insert
+  after reaching the maximum, the least recently used location will be removed.
+ */
+drop trigger if exists tr_ensure_maximum_recent_location on CustomerLocations cascade;
+create trigger tr_ensure_maximum_recent_location
+    after insert
+    on CustomerLocations
+    for each row
+execute function ensure_maximum_recent_location();
+
+create or replace function ensure_maximum_recent_location() returns trigger as
+$$
+begin
+    delete
+    from CustomerLocations
+    where (cid, lat, lon) in (
+        select cid, lat, lon
+        from CustomerLocations
+        where cid = new.cid
+        order by last_used_time desc
+            offset 5
+        limit 1);
+    return null;
+end ;
+$$ language plpgsql;
+
+/*
+ Ensures that all ordered foods for an order are from a single restaurant.
+ */
+create or replace function fn_order_food_from_same_restaurant() returns trigger as
+$$
+begin
+    if ((select count(distinct rid) from OrderFoods where oid = new.oid) <= 1) then
+        return null;
+    end if;
+    raise exception '% is from a different restaurant compared with other foods in this order.', new.food_name;
+end;
+$$ language plpgsql;
+
+drop trigger if exists tr_order_food_from_same_restaurant on OrderFoods cascade;
+create trigger tr_order_food_from_same_restaurant
+    after insert or update
+    on OrderFoods
+    for each row
+execute function fn_order_food_from_same_restaurant();
+
+/*
+  Ensures that non-overlapping and covering ISA relationship between Users and different user roles.
+ */
+create or replace function fn_ensure_covering_and_non_overlapping_roles() returns trigger as
+$$
+begin
+    if (select count(*)
+        from (select id
+              from Customers
+              union
+              select id
+              from Restaurants
+              union
+              select id
+              from Riders
+              union
+              select id
+              from Managers) as IDS
+        where IDS.id = new.id
+       ) = 1 then
+        return null;
+    end if;
+    raise exception 'User id % already has an other role or is not given a role.', new.id;
+end;
+$$ language plpgsql;
+
+drop trigger if exists tr_users_covering_role on Users cascade;
+create constraint trigger tr_users_covering_role
+    after insert or update
+    on Users
+    deferrable initially deferred
+    for each row
+execute function fn_ensure_covering_and_non_overlapping_roles();
+
+drop trigger if exists tr_restaurants_covering_role on Restaurants cascade;
+create trigger tr_restaurants_covering_role
+    after insert or update
+    on Restaurants
+    for each row
+execute function fn_ensure_covering_and_non_overlapping_roles();
+
+drop trigger if exists tr_customers_covering_role on Customers cascade;
+create trigger tr_customers_covering_role
+    after insert or update
+    on Customers
+    for each row
+execute function fn_ensure_covering_and_non_overlapping_roles();
+
+drop trigger if exists tr_riders_covering_role on Riders cascade;
+create trigger tr_riders_covering_role
+    after insert or update
+    on Riders
+    for each row
+execute function fn_ensure_covering_and_non_overlapping_roles();
+
+drop trigger if exists tr_managers_covering_role on Managers cascade;
+create trigger tr_managers_covering_role
+    after insert or update
+    on Managers
+    for each row
+execute function fn_ensure_covering_and_non_overlapping_roles();
