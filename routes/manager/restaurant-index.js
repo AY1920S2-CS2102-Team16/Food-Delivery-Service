331a8a8d8c88bc682d1edd6c00ee4d6cfbdecbb9
liujiajun <liujiajun1999@gmail.com>
2020-03-30 10:00:31 +0800

    Add manager index

---
+++ routes/manager/restaurant-index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../database/db");
+
+const sidebarItems = [
+    {name: "Dashboard", link: "/", icon: "tachometer-alt"},
+    {name: "Promotions", link: "/restaurant/promotions", icon: "percentage"},
+];
+
+router.all("*", function (req, res, next) {
+    if (!req.user || req.user.role !== "manager") {
+        return res.redirect("/");
+    } else {
+        next();
+    }
+});
+
+router.get("/", async function (req, res) {
+    res.render("pages/rider/rider-index", {sidebarItems: sidebarItems, user: req.user, navbarTitle: "Welcome"});
+});
+
+router.get("/promotions", async function (req, res) {
+    let actions, rules, promotions;
+    try {
+        rules = await db.any("select *, case when exists(select 1 from Managers where Managers.id = PromotionRules.giver_id) then true else false end as is_admin from PromotionRules where giver_id = $1 or exists(select 1 from Managers where Managers.id = giver_id)", req.user.id);
+        actions = await db.any("select *, case when exists(select 1 from Managers where Managers.id = PromotionActions.giver_id) then true else false end as is_admin from PromotionActions where giver_id = $1 or exists(select 1 from Managers where Managers.id = giver_id)", req.user.id);
+        promotions = await db.any("select *, case when exists(select 1 from Managers where Managers.id = Promotions.giver_id) then true else false end as is_admin from Promotions where giver_id = $1 or exists(select 1 from Managers where Managers.id = giver_id)", req.user.id);
+    } catch (e) {
+        console.log(e);
+    }
+    res.render("pages/restaurant/restaurant-promotions", {
+        sidebarItems: sidebarItems,
+        user: req.user,
+        navbarTitle: "Promotions",
+
+        rules: rules,
+        actions: actions,
+        promotions: promotions,
+        rtypes: ["ORDER_TOTAL", "NTH_ORDER"],
+        atypes: ["FOOD_DISCOUNT", "DELIVERY_DISCOUNT"], //TODO: Avoid hardcode values
+
+        successFlash: req.flash("success"),
+        errorFlash: req.flash("error")
+    });
+});
+
+router.post("/promotions/addrule", async function (req, res) {
+    try {
+        await db.none("insert into PromotionRules (giver_id, rtype, config) values ($1, $2, $3)",
+            [req.user.id, req.body.rtype, req.body.config]);
+        req.flash("success", "Rule added.");
+    } catch (e) {
+        console.log(e);
+        req.flash("error", "Error when adding rule.");
+    } finally {
+        res.redirect("/restaurant/promotions");
+    }
+});
+
+router.post("/promotions/addaction", async function (req, res) {
+    try {
+        await db.none("insert into PromotionActions (giver_id, atype, config) values ($1, $2, $3)",
+            [req.user.id, req.body.rtype, req.body.config]);
+        req.flash("success", "Action added.");
+    } catch (e) {
+        console.log(e);
+        req.flash("error", "Error when adding action.");
+    } finally {
+        res.redirect("/restaurant/promotions");
+    }
+});
+
+router.post("/promotions/addpromo", async function (req, res) {
+    try {
+        await db.none("insert into Promotions (promo_name, rule_id, action_id, start_time, end_time, giver_id) " +
+            "values ($1, $2, $3, $4, $5, $6)",
+            [req.body.desc, req.body.rule, req.body.action, req.body.start, req.body.end, req.user.id]);
+        req.flash("success", "Promotion added.");
+    } catch (e) {
+        console.log(e);
+        req.flash("error", "Error when adding promotion.");
+    } finally {
+        res.redirect("/restaurant/promotions");
+    }
+});
+
+router.get("/promotions/remove", async function (req, res) {
+    try {
+        if (req.query.actionid) {
+            await db.none("delete from PromotionActions where id = $1", req.query.actionid);
+        } else if (req.query.ruleid) {
+            await db.none("delete from PromotionRules where id = $1", req.query.ruleid);
+        } else if (req.query.promoid) {
+            await db.none("delete from Promotions where id = $1", req.query.promoid);
+        }
+    } catch (e) {
+        console.log(e);
+        req.flash("error", "Error when removing.");
+    } finally {
+        res.redirect("/restaurant/promotions");
+    }
+});
+module.exports = router;
